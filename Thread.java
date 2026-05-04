74ce93f607bcdac11d516a1e01d18521a2264949
Arian710 <shahidi.arian@gmail.com>
2026-05-05 01:19:15 +0200

    Rename to index.html for Vercel deployment

---
+++ Thread.java
@@ -0,0 +1,7 @@
+public class Thread{
+    public static void main(String[]args){
+        Threadnachricht tn1= new ThreadNachricht("Pr",1);
+        Thread thread1 = new Thread();
+        thread1.start();
+    }
+}
